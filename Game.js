17abd7e29eeafd163dcdfe292971e09958a1a6cd
d4rkstrife <p.gdc85@gmail.com>
2020-06-26 17:35:38 +0200

    methode fight

--- Game.js
+++ Game.js
@@ -5,16 +5,23 @@ class Game {
         this.round = 0;
         this.endGame = false;
         this.activePlayer;
+        this.passivePlayer;
     }
     init() {
         if (!this.endGame) {
 
-            let i = Math.floor(this.round % 2);
+            let i = Math.floor(this.round % 2)
+            let j = Math.floor((this.round+1) % 2)
             this.activePlayer = this.joueurs[i];
+            this.passivePlayer = this.joueurs[j];
             this.activePlayer.state = "active";
+            if (this.grille.state === "fight"){
+              this.fight(this.activePlayer, this.passivePlayer);
+            } else {
             this.activePlayer.jouer(this.grille);
+            }
             setInterval(() => { //boucle qui sert à savoir quand le joueur a joué, puis a passer au joueur suivant.
-                if (this.activePlayer.state == "Tour fini") {
+                if (this.activePlayer.state === "Tour fini") {
                     this.round++;
                     this.init();
                 }
@@ -23,7 +30,11 @@ class Game {
         }
 
     }
-
+  fight(activePlayer, passivePlayer){
+    console.log(activePlayer,passivePlayer);
+    activePlayer.attaquer(passivePlayer);
+    this.activePlayer.state = "Tour fini";
+  }
 
 
 }
