97baf151976102368ba45cb1e58ef37da328eb2e
d4rkstrife <p.gdc85@gmail.com>
2020-07-02 11:35:08 +0200

    refont systeme combat

--- Game.js
+++ Game.js
@@ -9,51 +9,21 @@ class Game {
     }
     init() {
         if (!this.endGame) {
-
-            let i = Math.floor(this.round % 2)
-            let j = Math.floor((this.round + 1) % 2)
+            let i = Math.floor(this.round % 2);
+            let j = Math.floor((this.round + 1) % 2);
             this.activePlayer = this.joueurs[i];
             this.passivePlayer = this.joueurs[j];
-            console.log(this.activePlayer)
             this.activePlayer.state = "active";
             if (this.grille.state === "fight") {
-                this.activePlayer.state = "combat";
-                this.fight(this.activePlayer, this.passivePlayer);
+                this.fight();
             } else {
-                this.activePlayer.seDeplacer(this.grille);
+                this.activePlayer.seDeplacer(this.grille, this);
             }
-            setInterval(() => { //boucle qui sert à savoir quand le joueur a joué, puis a passer au joueur suivant.
-                if (this.activePlayer.state === "Tour fini") {
-                    this.round++;
-                    console.log(this.activePlayer.pv, this.passivePlayer.pv);
-                    $(`#points_vie_${this.activePlayer.joueur}`).html(this.activePlayer.pv);
-                    $(`#points_vie_${this.passivePlayer.joueur}`).html(this.passivePlayer.pv);
-                    this.init();
-                } else if (this.activePlayer.state === "entre combat") {
-                    this.activePlayer.state = "combat";
-                    this.fight(this.activePlayer, this.passivePlayer);
-                }
-
-            }, 400);
-
-
         }
-
     }
-    fight(activePlayer, passivePlayer) {
-        activePlayer.positionCombat = "En attente";
-        activePlayer.choisirPostureCombat();
-        setInterval(() => { //boucle qui sert à savoir quand le joueur a choisi sa posture.
-            if (activePlayer.positionCombat === "attaque") {
-                activePlayer.attaquer(passivePlayer);
-
-                this.activePlayer.state = "Tour fini";
-
-            } else if (activePlayer.positionCombat === "défend") {
-                this.activePlayer.state = "Tour fini";
-            }
-        }, 100);
-
+    fight() {
+        console.log(this.activePlayer.damage)
+        this.activePlayer.choisirPostureCombat(this);
     }
 
 
--- Player.js
+++ Player.js
@@ -11,9 +11,9 @@ class Player {
         this.image = new Image();
         this.image.src = `image/${data[2]}`;
         this.state = "En attente";
-        this.positionCombat = "En préparation"
+        this.positionCombat = "défend";
     }
-    seDeplacer(grille) {
+    seDeplacer(grille, game) {
 
         let cases = [];
         let directions = ["droite", "gauche", "bas", "haut"];
@@ -23,47 +23,45 @@ class Player {
                 cases.push(grille.coordsToPosition(element));
             })
         })
-
-        let td = document.querySelectorAll('td');
-        td.forEach(element => {
-            element.addEventListener('click', () => {
-
-                if (this.state == "active" && cases.indexOf(parseInt(element.id)) !== -1) {
-                    let elementCoords = grille.positionToCoord(element.id);
+        let that = this;
+        $('td').each(function () {
+            $(this).one("click", () => {
+                if (that.state == "active" && cases.indexOf(parseInt(this.id)) !== -1) {
+                    let elementCoords = grille.positionToCoord(this.id);
                     if (grille.grille[elementCoords[0]][elementCoords[1]].statut == "case_vide") {
-                        grille.deplacerJoueur(this.coords, elementCoords);
-                        this.coords = elementCoords;
-                        this.position = grille.coordsToPosition(this.coords);
+                        grille.deplacerJoueur(that.coords, elementCoords);
+                        that.coords = elementCoords;
+                        that.position = grille.coordsToPosition(that.coords);
 
 
                     } else if (grille.grille[elementCoords[0]][elementCoords[1]].type == "arme") {
-                        this.equiperArme(grille.grille[elementCoords[0]][elementCoords[1]]);
-                        grille.deplacerJoueur(this.coords, elementCoords);
-                        this.coords = elementCoords;
-                        this.position = grille.coordsToPosition(this.coords);
+                        that.equiperArme(grille.grille[elementCoords[0]][elementCoords[1]]);
+                        grille.deplacerJoueur(that.coords, elementCoords);
+                        that.coords = elementCoords;
+                        that.position = grille.coordsToPosition(that.coords);
 
 
                     } else {
-                        alert("Avancez au corps à corps pour attaquer");
+                        alert("Pas encore prêt au combat");
 
                     }
+                    $('td').each(function () {
+                        $(this).removeClass('red');
+                        $(this).removeClass('green');
+                    })
                     if (grille.comparerPosition(grille.joueurs[0], grille.joueurs[1]) === true) {
                         grille.state = "fight";
-                        this.state = "entre combat"
+                        game.fight();
                     } else {
-                        this.state = "Tour fini";
+                        cases = [];
+                        that.state = "Tour fini";
+                        game.round++;
+                        game.init()
                     }
-                    console.log(grille.state)
-                    td.forEach(element => {
-                        element.classList.remove("red");
-                        element.classList.remove("green");
-                    })
-                    cases = [];
 
                 }
-
             })
-        })
+        });
     }
     equiperArme(emplacement) {
         let arme = emplacement.content
@@ -77,29 +75,41 @@ class Player {
         armeElt.attr("src", this.armeEquipee.image.src);
     }
     attaquer(joueur) {
-        if (joueur.positionCombat === "attaque") {
-            joueur.pv = Math.floor(joueur.pv - this.damage);
-        } else {
-            joueur.pv = Math.floor(joueur.pv - (this.damage / 2));
+        if (this.state === "active") {
+            if (joueur.positionCombat === "attaque") {
+                joueur.pv = Math.floor(joueur.pv - this.damage);
+            } else if (joueur.positionCombat === "défend") {
+                joueur.pv = Math.floor(joueur.pv - (this.damage / 2));
+            } else {
+                console.log("error")
+            }
         }
     }
-    choisirPostureCombat() {
-        this.positionCombat = "En attente";
-        $(`#fight_${this.joueur}`).show();
-        $(`#attack_button_${this.joueur}`).one('click', () => {
-            $(`#fight_${this.joueur}`).hide();
-            this.positionCombat = "attaque";
-            this.state = "Tour fini";
-            console.log(this.positionCombat)
-        });
-        $(`#def_button_${this.joueur}`).one('click', () => {
-            $(`#fight_${this.joueur}`).hide();
-            this.positionCombat = "défend";
-            this.state = "Tour fini";
-            console.log(this.positionCombat)
-        });
+    choisirPostureCombat(game) {
 
+        if (this.state === "active") {
+            $(`#fight_${this.joueur}`).show();
+            $(`#attack_button_${this.joueur}`).on('click', () => {
+                $(`#fight_${this.joueur}`).hide();
+                this.positionCombat = "attaque";
+                this.attaquer(game.passivePlayer);
+                $(`#points_vie_${game.activePlayer.joueur}`).html(game.activePlayer.pv);
+                $(`#points_vie_${game.passivePlayer.joueur}`).html(game.passivePlayer.pv);
+                this.state = "Tour fini";
+                game.round++;
+                game.init();
+            });
+            $(`#def_button_${this.joueur}`).on('click', () => {
+                $(`#fight_${this.joueur}`).hide();
+                this.positionCombat = "défend";
+                $(`#points_vie_${game.activePlayer.joueur}`).html(game.activePlayer.pv);
+                $(`#points_vie_${game.passivePlayer.joueur}`).html(game.passivePlayer.pv);
+                this.state = "Tour fini";
+                game.round++;
+                game.init();
+            });
 
+        }
 
     }
 }
